46269785d4a4ec921c5c13cffef2dcd6144aad79
1156721874 <1156721874@qq.com>
2016-09-26 10:41:33 +0800

    更换解题思路
    更换解题思路

--- leetCode/src/ceaser/_003LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
+++ leetCode/src/ceaser/_003LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
@@ -7,29 +7,30 @@ package ceaser._003LongestSubstringWithoutRepeatingCharacters;
 	Given "bbbbb", the answer is "b", with the length of 1.
 	Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
  * @author Administrator
+ reference : http://www.cnblogs.com/dollarzhaole/p/3155712.html
  *
  */
 public class LongestSubstringWithoutRepeatingCharacters {
 
 	public static void main(String[] args) {
-		String str = "err767abcdefghijkji";
-		int start = 0;
-		int end = 0;
-		String longestSubstring = "";
+		String str = "abcdefkkghzizja";
+		int idx = -1;
+		int max= 0;
+		int [] loca = new int[256];
 		char [] strArr = str.toCharArray();
 		for (int i = 0; i < strArr.length; i++) {
-			char leveA = strArr[i];
-			for (int j = i+1; j < strArr.length; j++) {
-				char levelB = strArr[j];
-				if(leveA==levelB){
-					String innerLongestSubstring = str.substring(i,j);
-					if(longestSubstring.length()<innerLongestSubstring.length()){
-						longestSubstring = innerLongestSubstring;
-					}
-				}
+			if(loca[strArr[i]]>idx){
+				idx = loca[strArr[i]];
 			}
+			if(i-idx>max){
+				max= i-idx;
+			}
+			loca[strArr[i]] = i;
 		}
-		System.out.println(longestSubstring);
+		System.out.println(max);
+		for (int i = 0; i < loca.length; i++) {
+			System.out.print(loca[i]);
+		}
 	}
 
 }
